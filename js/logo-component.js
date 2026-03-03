b49d83915877b85b09e345e729d29b82ecaa201b
Luis Leonardo Ruiz Nieto <leo@MacBook-Leo.local>
2026-03-02 23:11:50 -0600

    Actualización

--- js/logo-component.js
+++ js/logo-component.js
@@ -14,7 +14,7 @@ class MoviParkLogo extends HTMLElement {
                 .circle {
                     width: 100%;
                     height: 100%;
-                    background: #080808;
+                    background: #e6d626;
                     border-radius: 50%;
                     display: flex;
                     justify-content: center;
